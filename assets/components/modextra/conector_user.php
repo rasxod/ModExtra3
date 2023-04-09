3f7792b9d616b7bf7ddeaf52ae21b657297bfc6a
Nezabor <in001@z1q.ru>
2023-04-09 03:50:04 +0300

    Update conector_user.php
    reeedit

--- assets/components/modextra/conector_user.php
+++ assets/components/modextra/conector_user.php
@@ -1,7 +1,7 @@
 <?php
 
 /** @var  MODX\Revolution\modX $modx */
-/** @var  VoiVil\VoiVil $VoiVil */
+/** @var  ModExtra\ModExtra $ModExtra */
 
 if (file_exists(dirname(__FILE__, 4) . '/config.core.php')) {
     require_once dirname(__FILE__, 4) . '/config.core.php';
@@ -15,22 +15,21 @@ require_once MODX_CONNECTORS_PATH . 'index.php';
 
 header('Content-Type: application/json; charset=utf-8');
 
-if ($modx->user->get('id') == 0 || !str_contains($_REQUEST['action'], 'VoiVil\\Processors\\Voite\\')) {
+if ($modx->user->get('id') == 0 || !str_contains($_REQUEST['action'], 'ModExtra\\Processors\\ModExtra\\')) {
     echo json_encode(['success' => false, 'message' => 'Доступ запрещен']);
     die();
 }
 
-$VoiVil = $modx->services->get('VoiVil');
-$modx->lexicon->load('voivil:default');
+$ModExtra = $modx->services->get('ModExtra');
+$modx->lexicon->load('ModExtra:default');
 
 // handle request
 $path = $modx->getOption(
     'processorsPath',
     $VoiVil->config,
-    $modx->getOption('core_path') . 'components/voivil/' . 'Processors/'
+    $modx->getOption('core_path') . 'components/modextra/' . 'Processors/'
 );
 $data = $modx->runProcessor($_REQUEST['action'], $_REQUEST, ['processors_path' => $path])->getResponse();
-// $modx->log(1, $_REQUEST['action']);
 if (is_array($data)) {
     echo json_encode($data);
 } else {
